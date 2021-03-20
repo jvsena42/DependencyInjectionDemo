aa71ae978057aeba5c9625d9d5f79754ed240f10
jvsena42 <jvsena16@gmail.com>
2021-03-20 07:50:01 -0300

    [ADD] MainActivity.kt / SmartPhoneComponent.kt - Field injection

--- app/src/main/java/com/anushka/didemo/MainActivity.kt
+++ app/src/main/java/com/anushka/didemo/MainActivity.kt
@@ -2,16 +2,26 @@ package com.anushka.didemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
-private lateinit var smartPhone: SmartPhone
+
+    @Inject
+    lateinit var smartPhone: SmartPhone
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        DaggerSmartPhoneComponent.create()
+        /*DaggerSmartPhoneComponent.create()
             .getSmartPhone()
-            .makeACallWithRecording()
-        
+            .makeACallWithRecording()*/
+
+        DaggerSmartPhoneComponent.create()
+            .inject(this)
+
+        smartPhone.makeACallWithRecording()
+
     }
 }
--- app/src/main/java/com/anushka/didemo/SmartPhoneComponent.kt
+++ app/src/main/java/com/anushka/didemo/SmartPhoneComponent.kt
@@ -4,5 +4,7 @@ import dagger.Component
 
 @Component(modules = [MemoryCardModule::class,NCBatteryModule::class])
 interface SmartPhoneComponent {
-   fun getSmartPhone() : SmartPhone
+//   fun getSmartPhone() : SmartPhone
+   /*Serve para não precisar criar uma função get e chamar na activity*/
+   fun inject(mainActivity: MainActivity)
 }
